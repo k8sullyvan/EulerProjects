9a2e0a75f04413cd9dcb66fc37a1da2e015325fa
Caitlin O'Sullivan <caitlineosullivan@gmail.com>
2016-01-23 13:11:32 +0000

    Edit to tabulation

--- Haskell Solutions/002_EvenFibonacci.hs
+++ Haskell Solutions/002_EvenFibonacci.hs
@@ -1,8 +1,8 @@
 -- Where m is the max value and s is the current sum
-sumEvnFib			:: Int -> (Int, Int, Int) -> Int
+sumEvnFib	:: Int -> (Int, Int, Int) -> Int
 sumEvnFib m (a,b,s) = if a+b<m 
-						then sumEvnFib m (a+2*b, ((2*a)+(3*b)), s+a+b)
-						else s
+			then sumEvnFib m (a+2*b, ((2*a)+(3*b)), s+a+b)
+			else s
 
 --Starting at 0 with the max set at 4,000,000
 -- sumEvnFib 4000000 (1, 1, 0)
