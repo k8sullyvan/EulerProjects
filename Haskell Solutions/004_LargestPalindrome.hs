0e7f5c9eb93848dbc8cdcc133333abe22a55810f
Caitlin O'Sullivan <caitlineosullivan@gmail.com>
2016-01-26 11:06:12 +0000

    decides if a number is palindromic or not

---
+++ Haskell Solutions/004_LargestPalindrome.hs
@@ -0,0 +1,15 @@
+isPalin	   :: [Int] -> Bool
+isPalin []  = True
+isPalin [x] = True
+isPalin (x:xs) 	= x == (last xs) && isPalin (init xs)
+
+-- Returns the number as a list in reverse order
+-- Note Palindromic numbers are not effected by this
+intList  :: Int -> [Int]
+intList 0 = []
+intList x = mod x 10 : intList (div x 10)
+
+
+isPalindrome :: Int -> Bool
+isPalindrome x = isPalin (intList x)
+
